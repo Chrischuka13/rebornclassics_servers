cabc61243bc0f6c5f499be1e4c2dc9029e5e271b
Chrischuka13 <divineprovidence30@gmail.com>
2026-09-09 14:46:46 +0100

    vercel url added

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
   'http://localhost:5174',
+  'https://rebornclothing.vercel.app'
 ].filter(Boolean) as string[];
 
 app.use(cors({
